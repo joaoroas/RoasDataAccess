cf9adce3d990259b1ccd94c662e68034cd612fe6
João Roas <jr.roas@gmail.com>
2023-01-09 16:19:51 -0300

    Mapeamento

---
+++ Models/CareerItem.cs
@@ -0,0 +1,11 @@
+
+
+namespace RoasDataAccess.Models
+{
+    public class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,10 @@
+
+
+namespace RoasDataAccess.Models
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
